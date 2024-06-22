cc33458c20f23727830006bc767dcbb9de7f4a9b
Jinlei Li <jinleili0@outlook.com>
2024-06-22 19:24:24 +0800

    Reduce `.wasm` size

--- build-wasm.sh
+++ build-wasm.sh
@@ -1,12 +1,13 @@
-RUSTFLAGS=--cfg=web_sys_unstable_apis cargo build --no-default-features --release --target wasm32-unknown-unknown \
---bin simuverse
+cargo build --no-default-features --profile wasm-release --target wasm32-unknown-unknown --bin simuverse
 
 # Generate bindings
-for i in target/wasm32-unknown-unknown/release/*.wasm;
+for i in target/wasm32-unknown-unknown/wasm-release/*.wasm;
 do
     wasm-bindgen --no-typescript --out-dir wasm --web "$i";
+    # 优化 wasm 包大小
+    # 2024/5/5, 4.6MB -> 3.2MB
+    # 2024/6/22, 3.2MB -> 2.2MB
+    filename=$(basename "$i");
+    name_no_extension="${filename%.wasm}";
+    wasm-opt -Oz --output wasm/"$name_no_extension"_optimized_bg.wasm wasm/"$name_no_extension"_bg.wasm
 done
-
-# 优化 wasm 包大小
-# 2024/5/5, 4.6MB -> 3.2MB
-wasm-opt -Oz --output wasm/optimized.wasm wasm/simuverse_bg.wasm
